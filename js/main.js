3849f0b0762f63517648e58d8c192607b61c263f
Darren Le <darrenle98@gmail.com>
2019-02-16 01:31:56 -0500

    added drag and drop funciton

--- js/main.js
+++ js/main.js
@@ -4,55 +4,53 @@
 
 
 	let instruments = document.querySelectorAll(".instruments");
+	let audio = document.querySelectorAll("audio")
 	let button = document.querySelectorAll("button");
-	console.log(button);
 	
 	instruments.forEach(Off => Off.volume = 0);
-	
-	button[0].addEventListener("click", function()
-	{
-		instruments[0].volume = 1;
-		console.log(button[0]);
-
-	});
-
-	button[1].addEventListener("click", function()
-	{
-		instruments[1].volume = 1;
-		console.log(button[1]);
-
-	});
+	let icons = document.querySelector(".icons")
+	console.log(icons)
+	let dropzone = document.getElementById("worldMap")
+	console.log(dropzone)
 
 
-	button[2].addEventListener("click", function()
-	{
-		instruments[2].volume = 1;
-		console.log(button[2]);
-
-	});
-
-
-	button[3].addEventListener("click", function()
-	{
-		instruments[3].volume = 1;
-		console.log(button[3]);
-
-	});
-
-	button[4].addEventListener("click", function()
-	{
-		instruments[4].volume = 1;
-		console.log(button[4]);
-
-	});
 	
 
+			icons.querySelectorAll("img").forEach(img => img.addEventListener("dragstart", function o(e){
+				console.log('draggin...');
+				e.dataTransfer.setData("text/plain", this.dataset.instrumentsref);
+				console.log(this.dataset.instrumentsref)
+				
+				
+			}))
 
-	button[5].addEventListener("click", function()
-	{
-		instruments[5].volume = 1;
-		console.log(button[5]);
 
-	});
+	
+
+				// instruments[iconAudio].volume = 1;
+
+			
+
+	dropzone.addEventListener("dragover", function(e){
+			e.preventDefault();
+			console.log('dragged over me!')
+		}); 
+
+		dropzone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			let dataDrop = e.dataTransfer.getData("text/plain");
+			console.log(dataDrop)
+				instruments[dataDrop].volume = 1;
+			console.log('you dropped something on me');
+		})
+
+	
+
+
+	
+	
+
+
+	
 
 	})();
