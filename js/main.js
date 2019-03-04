83a08d3840840b167654c106f2c891c067c73643
Darren Le <darrenle98@gmail.com>
2019-03-04 11:14:39 -0500

    enhance drop function

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 	let instruments = document.querySelectorAll(".instruments");
 	let audio = document.querySelectorAll("audio")
 	let button = document.querySelectorAll("button");
-	
+	let waveZone = document.querySelector(".pulsing_Circles")
 	instruments.forEach(Off => Off.volume = 0);
 	let icons = document.querySelector(".icons")
 	console.log(icons)
@@ -21,15 +21,23 @@
 				e.dataTransfer.setData("text/plain", this.dataset.instrumentsref);
 				console.log(this.dataset.instrumentsref)
 				
+			 	console.log(this)
+			 	console.log(img)
+
+
+	
+				
 				
 			}))
 
 
 	
 
-				// instruments[iconAudio].volume = 1;
 
-			
+	
+
+	
+
 
 	dropzone.addEventListener("dragover", function(e){
 			e.preventDefault();
@@ -42,6 +50,16 @@
 			console.log(dataDrop)
 				instruments[dataDrop].volume = 1;
 			console.log('you dropped something on me');
+			let newWave = `<img class="wave${e.dataTransfer.getData("text/plain")}" class="wave-image"
+			 	src="images/${"wave"+ e.dataTransfer.getData("text/plain")}.svg" alt="Wave" draggable>`;
+			 	waveZone.innerHTML += newWave;
+			 	console.log(newWave);
+			let newMap = `<img class="map${e.dataTransfer.getData("text/plain")} map" class="wave-image"
+			 	src="images/${"map"+ e.dataTransfer.getData("text/plain")}.svg" alt="Wave" draggable>`;
+			 	console.log(newMap);
+
+			 	dropzone.innerHTML += newMap ;
+			 	
 		})
 
 	
